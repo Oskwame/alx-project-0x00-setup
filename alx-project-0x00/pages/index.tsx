2d8e26a708ef1a55502ee54d5b815dec69d4eabc
oskwame <nanaqwamee124@gmail.com>
2025-11-13 03:39:55 +0000

    Updated the index.tsx file

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home: React.FC = () => {
   return(
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-bold">
-      <h1>Airn Application Clone System</h1>
+      <h1>Airbnb Application Clone System</h1>
       <button className="border px-3 py-1 text-lg mt-5 bg-blue-900 text-white rounded hover:bg-blue-700">
         Get Started
       </button>
