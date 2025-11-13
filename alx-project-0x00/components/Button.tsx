1419206649f317b975fac7ee1501692bca3654ad
oskwame <nanaqwamee124@gmail.com>
2025-11-13 12:19:57 +0000

    added  pure react button component with size and shape variants

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+const Button: React.FC<ButtonProps> = ({
+    title,
+    size = 'medium',
+    shape = 'rounded-md',
+    className = '',
+    style,
+    ...props
+
+}) => {
+    const sizeClasses: Record<string, String> ={
+    small: "px-3 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+    };
+
+    const shapeClasses: Record<string, String> ={
+        "rounded-sm": "rounded-sm",
+        "rounded-md": "rounded-md",
+        "rounded-full": "rounded-full",
+    };
+    return(
+        <button
+        className={`bg-blue-600 text-white font-medium ${sizeClasses[size]} ${shapeClasses[shape]} hover:bg-blue-700 transition ${className}`}
+        style={style}
+        {...props}
+    >
+        {title}
+
+        </button>
+    );
+};
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 const Landing: React.FC= () =>{
     return(
         <div>
-            <h1 className=" text-2xl font-extralight">Landing Page</h1>
-            <Card />
-        </div>
-      )
+            <h1 className=" text-2xl font-extralight">bUTTON COMPONENT DEMO</h1>
+
+              {/* Shape Variants */}
+      <div className="flex flex-wrap gap-4 justify-center">
+        <Button title="Rounded Small" shape="rounded-sm" size="medium" />
+        <Button title="Rounded Medium" shape="rounded-md" size="medium" />
+        <Button title="Rounded Full" shape="rounded-full" size="medium" />
+      </div>
+
+      {/* Size Variants */}
+      <div className="flex flex-wrap gap-4 justify-center mt-8">
+        <Button title="Small Button" shape="rounded-md" size="small" className="bg-amber-700" />
+        <Button title="Medium Button" shape="rounded-md" size="medium" />
+        <Button title="Large Button" shape="rounded-md" size="large" />
+      </div>
+
+      {/* Custom Style Example */}
+      <div className="flex gap-4 mt-8">
+        <Button
+          title="Success"
+          size="medium"
+          shape="rounded-full"
+          className="bg-green-600 hover:bg-green-700"
+        />
+        <Button
+          title="Danger"
+          size="medium"
+          shape="rounded-full"
+          className="bg-red-600 hover:bg-red-700"
+        />
+      </div>
+    </div>
+  );
 }
+
 export default Landing
