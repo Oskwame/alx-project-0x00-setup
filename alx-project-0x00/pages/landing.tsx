9b663cb3e7dc3b90e62c5b29ba4d3620a23f99eb
oskwame <nanaqwamee124@gmail.com>
2025-11-13 10:23:48 +0000

    landing page updated

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -3,7 +3,6 @@ import Card from "@/components/Card";
 const Landing: React.FC= () =>{
     return(
         <div>
-            <h1 className=" text-2xl font-extralight">Landing Page</h1>
             <Card/>
         </div>
       )
